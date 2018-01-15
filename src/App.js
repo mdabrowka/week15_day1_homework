ba1441e676cc27809b04a4830cdb9a2306ffb771
Marta <martadabrowka@gmail.com>
2018-01-15 20:27:56 +0000

    added OpeningsBox to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import OpeningsBox from './components/OpeningsBox.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <OpeningsBox />
     );
   }
 }
